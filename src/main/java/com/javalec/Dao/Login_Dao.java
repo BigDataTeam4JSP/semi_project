e84a203030998adf5aa7bc77c8ea6a14604dd9ba
ssujjy <ssujjy@gmail.com>
2024-01-29 12:56:28 +0900

    이동

---
+++ src/main/java/com/javalec/Dao/Login_Dao.java
@@ -0,0 +1,60 @@
+package com.javalec.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class Login_Dao {
+	// field
+	DataSource dataSource;
+	
+	// Constructor
+	public Login_Dao() {
+		// TODO Auto-generated constructor stub
+		try{
+			Context context = new InitialContext();
+			dataSource = (DataSource)context.lookup("java:comp/env/jdbc/shoes"); 	//[java:comp/env]는 context.xml의 위치. [jdbc/shoes]	는 이름. 		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Method
+	//checkID
+	public boolean checkLogin(String id, String pw) {
+		Connection conn = null;
+		PreparedStatement ps = null;	
+		ResultSet rs = null;
+		
+		try {
+			conn = dataSource.getConnection();	// DB연결. 
+			
+			String query = "SELECT id, pw FROM member ";
+			String where = " WHERE id = '" + id +"' AND pw = '" + pw +"'";
+			
+			ps = conn.prepareStatement(query+where);
+			rs = ps.executeQuery();
+
+			if(rs.next()) {
+				return true;
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {	
+			try {
+				if(rs != null) rs.close();
+				if(ps != null) ps.close();
+				if(conn != null) conn.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}//checkID()
+	
+}
