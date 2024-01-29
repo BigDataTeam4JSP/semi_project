ac05aa2e927de3aad1193c1e78df59172b032a5a
ssujjy <ssujjy@gmail.com>
2024-01-29 14:28:00 +0900

    다른 파일 수정된것 올라감

---
+++ src/main/java/com/javalec/Dao/productList_Dao.java
@@ -0,0 +1,78 @@
+package com.javalec.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.javalec.Dto.productList_Dto;
+
+
+public class productList_Dao {
+	
+	//Field
+	DataSource dataSource;
+	
+	//Constructor
+	
+	public productList_Dao() {		
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/shoes");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Method
+	
+	public ArrayList<productList_Dto> list() {
+		
+		ArrayList<productList_Dto> dtos = new ArrayList<productList_Dto>();
+	
+	Connection connection = null;
+	PreparedStatement preparedStatement = null;
+	ResultSet resultset = null;
+	 
+		try {
+			
+			connection = dataSource.getConnection();	
+			String query ="SELECT pid, pname FROM product";			
+			preparedStatement = connection.prepareStatement(query);
+			resultset = preparedStatement.executeQuery();
+
+			while (resultset.next()) {
+				
+				int pid = resultset.getInt("pid"); 
+				String pname = resultset.getString("pname"); 
+				
+				
+				productList_Dto dto = new productList_Dto (pid,pname);
+
+				dtos.add(dto);
+			}	
+                      
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally { // 데이터 정리하는 용도로 쓰임 (만든 순서 거꾸로 정리해야함)
+			try {
+				if (resultset != null) {
+					resultset.close();
+				}
+				if (preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (Exception e) {
+
+			}
+		}
+		return dtos;
+	}
+	
+}
--- src/main/java/com/javalec/command/aProcuctQtyUpdate.java
+++ src/main/java/com/javalec/command/aProcuctQtyUpdate.java
@@ -3,12 +3,21 @@ package com.javalec.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javalec.Dao.aProduct_Dao;
+
 public class aProcuctQtyUpdate implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-
+		int id = Integer.parseInt(request.getParameter("pid"));
+		int size = Integer.parseInt(request.getParameter("psize"));
+		int qty = Integer.parseInt(request.getParameter("pqty"));
+		
+		System.out.println(id+" " +size+" "+qty);
+		
+		aProduct_Dao dao = new aProduct_Dao();
+		dao.update(id, size, qty);
 	}
 
 	@Override
